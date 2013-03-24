3ed7fbbe19eec24752c827720a5b7a945af86335
Ian Lee <IanLee1521@gmail.com>
2013-03-24 02:28:02 -0700

    Updated some early files.

---
+++ zenith/src/loan.cpp
@@ -0,0 +1,20 @@
+/**
+  * foo
+  *
+  * @author Ian Lee
+  */
+
+class Loan {
+public:
+  Loan();
+  ~Loan();
+
+private:
+  float principle;
+  float interest_rate;
+}
+
+int main() {
+
+  return 0;
+}
